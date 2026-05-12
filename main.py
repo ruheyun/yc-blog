2136302849b6949b0d6f4d449065d6a697192efe
ruheyun <123306854+ruheyun@users.noreply.github.com>
2026-05-12 21:43:20 +0800

    Update blog description for clarity and translation

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from lxml.etree import CDATA
 from marko.ext.gfm import gfm as marko
 
 MD_HEAD = """## [yc-blog](https://ruheyun.github.io/yc-blog)
-My personal blog([About Me](https://github.com/ruheyun/yc-blog/issues/4)) using issues and GitHub Actions (随意转载，无需署名) [RSS Feed](https://raw.githubusercontent.com/{repo_name}/master/feed.xml)
+My personal blog([About](https://github.com/ruheyun/yc-blog/issues/4)) using issues and GitHub Actions (Free to share, no attribution required.) [RSS Feed](https://raw.githubusercontent.com/{repo_name}/master/feed.xml)
 """
 
 BACKUP_DIR = "BACKUP"
